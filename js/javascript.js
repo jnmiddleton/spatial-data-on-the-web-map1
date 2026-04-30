b1c18f0dede998ae01d2d83fc88304c2d3866112
jnmiddleton <jessicanicole@ut.ee>
2026-04-30 19:05:17 +0300

    finish web application setup tutorial v1

--- js/javascript.js
+++ js/javascript.js
@@ -42,3 +42,34 @@ async function addDistrictsGeoJson(url) {
   })
   polygons.addTo(map)
 }
+
+// add geoJSON points layer*
+async function addCelltowersGeoJson(url) {
+  const response = await fetch(url)
+  const data = await response.json()
+  const markers = L.geoJson(data)
+  markers.addTo(map)
+}
+addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+
+// cell tower markers 
+function createCircle(feature, latlng) {
+  let options = {
+    radius: 5,
+    fillColor: 'red',
+    fillOpacity: 0.5,
+    color: 'red',
+    weight: 1,
+    opacity: 1,
+  }
+  return L.circleMarker(latlng, options)
+}
+
+async function addCelltowersGeoJson(url) {
+  const response = await fetch(url)
+  const data = await response.json()
+  const circles = L.geoJson(data, {
+    pointToLayer: createCircle,
+  })
+  circles.addTo(map)
+}
